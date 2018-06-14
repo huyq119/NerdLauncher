f27b6a224451efc38e786ef3bac687bcec9bc6eb
huyuqi <yuqi.hU@gmail.com>
2018-06-14 10:25:16 +0800

    创建显式Intent 添加创新新任务tag

--- app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
@@ -79,7 +79,7 @@ public class NerdLauncherFragment extends Fragment {
 
             Intent i = new Intent(Intent.ACTION_MAIN)
                     .setClassName(activityInfo.applicationInfo.packageName,
-                            activityInfo.name);
+                            activityInfo.name).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(i);
 
         }
